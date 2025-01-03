2e293d339399211297948a44f740612549f43ec6
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2025-01-03 20:50:48 +0500

    l

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { BlogPost } from '../types/blog';
 import { formatDate } from '../utils/blog';
 import Newsletter from './Newsletter';
-
+import { post } from '../data/post'
 interface PostProps {
   post: BlogPost;
 }
