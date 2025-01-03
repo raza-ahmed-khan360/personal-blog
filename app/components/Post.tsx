1a9994d6bc60b53dd93b71a03d282e1d232aba81
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2025-01-03 21:02:36 +0500

    .

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -3,7 +3,7 @@ import Post from '../post/Post';
 export default function Post({ post }: PostProps) {
   return (
     <article className="min-h-screen pt-16">
-      <Post/>
+      <Post />
     </article>
   );
 } 
